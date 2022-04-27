376b1e426093ff729dd31d38025d5fc7dcf9cbcd
Nguyễn Mạnh Toàn <79891657+Kin2k1@users.noreply.github.com>
2022-04-27 15:28:15 +0700

    test pygame

--- test.py
+++ test.py
@@ -11,6 +11,11 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    DISPLAYSURF.fill((255, 255, 255))
-    pygame.draw.rect(DISPLAYSURF, (255, 0, 0), (100, 80, 150, 50))
+    DISPLAYSURF.fill((255, 255, 255))#Tô màu nền
+    
+    surface2rect = pygame.Surface((150, 50))
+    surface2rect.fill((0, 255, 0))
+    pygame.draw.rect(surface2rect, (255, 0, 0), (20, 20, 50, 20))
+    DISPLAYSURF.blit(surface2rect, (100, 80))
+    
     pygame.display.update()
