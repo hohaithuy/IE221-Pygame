22cb4a605a27d3fb42e6c1257c74b96a68d8dbf2
Hồ Hải Thủy <HHT.ThuyHH@gmail.com>
2022-05-03 14:27:40 +0700

    Fix path

--- MeowAdventure/Agent/agent.py
+++ MeowAdventure/Agent/agent.py
@@ -44,13 +44,13 @@ class Cat(agent):
     def __init__(self, hp, dmg):
         
         path_cat = "./MeowAdventure/Agent/MeowKnight/"
-        self.suface = {'right' : [pygame.transform.scale(pygame.image.load(path_cat + "run/" + i), (200,200)) for i in os.listdir(path_cat + "run") ]
-                        ,'idle' : [pygame.transform.scale(pygame.image.load(path_cat + "idle/" + i), (200,200)) for i in os.listdir(path_cat + "idle") ]
-                        ,'jump' : [pygame.transform.scale(pygame.image.load(path_cat + "jump/" + i), (200,200)) for i in os.listdir(path_cat + "jump") ]
-                        ,'attack1' : [pygame.transform.scale(pygame.image.load(path_cat + "attack1/" + i), (200,200)) for i in os.listdir(path_cat + "attack1") ]
-                        ,'attack2' : [pygame.transform.scale(pygame.image.load(path_cat + "attack2/" + i), (200,200)) for i in os.listdir(path_cat + "attack2") ]
-                        ,'takeDmg' : [pygame.transform.scale(pygame.image.load(path_cat + "takeDmg/" + i), (200,200)) for i in os.listdir(path_cat + "takeDmg") ]
-                        ,'death' : [pygame.transform.scale(pygame.image.load(path_cat + "death/" + i), (200,200)) for i in os.listdir(path_cat + "death") ]
+        self.suface = {'right' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "run/", i)), (200,200)) for i in os.listdir(os.path.join("MeowKnight", "run")) ]
+                        ,'idle' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "idle/", i)), (200,200)) for i in os.listdir(os.path.join("MeowKnight", "idle")) ]
+                        ,'jump' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "jump/", i)), (200,200)) for i in os.listdir(os.path.join("MeowKnight", "jump")) ]
+                        ,'attack1' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "attack1/", i)), (200,200)) for i in os.listdir(os.path.join("MeowKnight", "attack1")) ]
+                        ,'attack2' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "attack2/", i)), (200,200)) for i in os.listdir(os.path.join("MeowKnight", "attack2")) ]
+                        ,'takeDmg' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "takeDmg/", i)), (200,200)) for i in os.listdir(os.path.join("MeowKnight", "takeDmg")) ]
+                        ,'death' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "death/", i)), (200,200)) for i in os.listdir(os.path.join("MeowKnight", "death")) ]
                         }
         self.suface['left'] = [pygame.transform.flip(i, True, False) for i in self.suface['right'] ]
         super().__init__(200, 200, hp, dmg)
@@ -71,12 +71,18 @@ class Cat(agent):
                 self.setY(self.y - 2) # tăng từ hoạt ảnh 1 - 5, giảm sau 6 hành động sau- đang xử lý
             else:
                 self.setY(self.y + 2)
-            
-        screen.blit(self.suface[action][int(self.action)], (self.x, self.y))
+        
+        char = self.suface[action][int(self.action)]
+        charRect = char.get_rect(midbottom = (self.x, self.y))
+
+        screen.blit(char, charRect)
         
         self.action += speedGame
         if self.action >= len(self.suface[action]):
             self.action = 0
+    
+    def jump(self):
+        return 0
             
 
 WIDTH, HEIGHT = 900, 500
@@ -95,7 +101,6 @@ def main():
     action = 'idle' #Hành động của nhân vật
     
     while run:
-        print(action)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
