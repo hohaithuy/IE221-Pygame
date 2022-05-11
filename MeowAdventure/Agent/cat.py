a6eb2ff0c22bdde35189b9cd765fd2f4eac21255
Hồ Hải Thủy <HHT.ThuyHH@gmail.com>
2022-05-11 16:56:15 +0700

    Finish player almost

--- MeowAdventure/Agent/cat.py
+++ MeowAdventure/Agent/cat.py
@@ -8,13 +8,13 @@ class Cat(agent):
     def __init__(self, hp=10, dmg=1, W_Screen = 900, H_Screen = 500):
         agent.__init__(self, hp, dmg, W_Screen, H_Screen)
 
-        self.suface = {'run' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "run/", i)).convert_alpha(), (100,100)) for i in os.listdir(os.path.join("MeowKnight", "run")) ]
-                        ,'idle' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "idle/", i)).convert_alpha(), (100,100)) for i in os.listdir(os.path.join("MeowKnight", "idle")) ]
-                        ,'jump' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "jump/", i)).convert_alpha(), (100,100)) for i in os.listdir(os.path.join("MeowKnight", "jump")) ]
-                        ,'attack1' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "attack1/", i)).convert_alpha(), (100,100)) for i in os.listdir(os.path.join("MeowKnight", "attack1")) ]
-                        ,'attack2' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "attack2/", i)).convert_alpha(), (100,100)) for i in os.listdir(os.path.join("MeowKnight", "attack2")) ]
-                        ,'takeDmg' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "takeDmg/", i)).convert_alpha(), (100,100)) for i in os.listdir(os.path.join("MeowKnight", "takeDmg")) ]
-                        ,'death' : [pygame.transform.scale(pygame.image.load(os.path.join("MeowKnight", "death/", i)).convert_alpha(), (100,100)) for i in os.listdir(os.path.join("MeowKnight", "death")) ]
+        self.suface = {'run' : [pygame.transform.scale2x(pygame.image.load(os.path.join("MeowKnight", "run/", i)).convert_alpha()) for i in os.listdir(os.path.join("MeowKnight", "run")) ]
+                        ,'idle' : [pygame.transform.scale2x(pygame.image.load(os.path.join("MeowKnight", "idle/", i)).convert_alpha()) for i in os.listdir(os.path.join("MeowKnight", "idle")) ]
+                        ,'jump' : [pygame.transform.scale2x(pygame.image.load(os.path.join("MeowKnight", "jump/", i)).convert_alpha()) for i in os.listdir(os.path.join("MeowKnight", "jump")) ]
+                        ,'attack1' : [pygame.transform.scale2x(pygame.image.load(os.path.join("MeowKnight", "attack1/", i)).convert_alpha()) for i in os.listdir(os.path.join("MeowKnight", "attack1")) ]
+                        ,'attack2' : [pygame.transform.scale2x(pygame.image.load(os.path.join("MeowKnight", "attack2/", i)).convert_alpha()) for i in os.listdir(os.path.join("MeowKnight", "attack2")) ]
+                        ,'takeDmg' : [pygame.transform.scale2x(pygame.image.load(os.path.join("MeowKnight", "takeDmg/", i)).convert_alpha()) for i in os.listdir(os.path.join("MeowKnight", "takeDmg")) ]
+                        ,'death' : [pygame.transform.scale2x(pygame.image.load(os.path.join("MeowKnight", "death/", i)).convert_alpha()) for i in os.listdir(os.path.join("MeowKnight", "death")) ]
                         }
         
         self.action = 'idle'
@@ -24,12 +24,12 @@ class Cat(agent):
         self.index = 0
         self.image = self.suface['idle'][int(self.index)]
         self.rect = self.image.get_rect(midbottom = (self.x, self.y))
-        self.framrate = 0.2
+        self.framerate = 0.2
         self.isJump = False
         self.jumpCount = 15  
         self.vel = 0
         self.flip = False
-    
+        self.attack = False
     def setAction(self):
         #reset hành động mỗi lần bấm phím
         self.action = 0
@@ -46,6 +46,14 @@ class Cat(agent):
         elif keys[pygame.K_LEFT]:
             self.setX(self.x - (self.vel + 1))
             self.flip = True
+        elif keys[pygame.K_a] and not self.attack:
+            self.action = 'attack1'
+            self.attack = True
+            self.index = 0
+        elif keys[pygame.K_s] and not self.attack:
+            self.action = 'attack2'
+            self.attack = True
+            self.index = 0
 
     def jump(self):
         if self.isJump:
@@ -60,7 +68,7 @@ class Cat(agent):
                 self.action = 'idle'
                 self.isJump = False
                 self.jumpCount = 15
-
+        
     def apply_velocity(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
@@ -69,12 +77,20 @@ class Cat(agent):
                 self.action = 'run'
         else:
             self.vel = 0
-            self.action = 'idle'
         if self.vel > 3: self.vel = 4
     
     def animations_state(self):
-        self.index += self.framrate
+        if self.attack:
+            self.framerate = 2 / (20 - len(self.suface[self.action]))
+            print(self.framerate)
+
+
+        self.index += self.framerate
         if self.index >= len(self.suface[self.action]):
+            if self.attack:
+                self.framerate = 0.2
+                self.action = 'idle'
+                self.attack = False
             self.index = 0
 
         self.image = self.suface[self.action][int(self.index)]
