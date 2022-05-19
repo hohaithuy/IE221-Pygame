15564a4c4cf43fb9af9a9f9f7c3a2846b5c9a0bf
Nguyễn Mạnh Toàn <79891657+Kin2k1@users.noreply.github.com>
2022-05-19 12:29:29 +0700

    add golem

--- MeowAdventure/Agent/enemy.py
+++ MeowAdventure/Agent/enemy.py
@@ -67,13 +67,13 @@ class Enemy(pygame.sprite.Sprite):
         
 class Frog(Enemy):
     
-    def __init__(self, screen, player, x, y,  hp = 5, dmg= 1, W_Screen = 900, H_Screen = 500, action_name = "idle"):  
+    def __init__(self, screen, player, x, y,  hp = 10, dmg= 2, action_name = "idle"):  
         """_summary_
 
         Args:
             attack = hop + attack + death.
         """
-        super().__init__(player, x, y, hp, dmg, W_Screen, H_Screen)
+        super().__init__(player, x, y, hp, dmg)
         self.screen = screen
         self.suface = {'run' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Frog", "run/", i)).convert_alpha()) for i in os.listdir(os.path.join("Frog", "run")) ]
                         ,'idle' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Frog", "idle/", i)).convert_alpha()) for i in os.listdir(os.path.join("Frog", "idle")) ]
@@ -157,7 +157,8 @@ class Frog(Enemy):
         
     def attackAction(self):
         sprite = self.player.sprites()[0]
-        if abs(self.x - sprite.rect.right) <= 35 or abs(self.x - sprite.rect.left) <= 35:
+        if (abs(self.x - sprite.rect.right) <= 35 or abs(self.x - sprite.rect.left) <= 35) and self.rect.center[1] <= sprite.rect.bottom and self.rect.center[1] >= sprite.rect.top:
+            
             self.attackDmg(sprite)  
     
     def checkCollide(self):
@@ -188,13 +189,13 @@ class Frog(Enemy):
 ########################
 class Slime(Enemy):
     
-    def __init__(self, screen, player, x, y, hp = 3, dmg= 1, W_Screen = 900, H_Screen = 500, action_name = "idle"):  
+    def __init__(self, screen, player, x, y, hp = 7, dmg = 1, action_name = "idle"):  
         """_summary_
 
         Args:
             attack = hop + attack + death.
         """
-        super().__init__(player, x, y, hp, dmg, W_Screen, H_Screen)
+        super().__init__(player, x, y, hp, dmg)
         self.screen = screen
         self.suface = {'idle' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Slime", "idle/", i))) for i in os.listdir(os.path.join("Slime", "idle")) ]
                         ,'attack' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Slime", "hop/", i))) for i in os.listdir(os.path.join("Slime", "hop")) ]
@@ -255,7 +256,7 @@ class Slime(Enemy):
         
     def attackAction(self):
         sprite = self.player.sprites()[0]
-        if abs(self.x - sprite.rect.right) <= 45 or abs(self.x - sprite.rect.left) <= 45:
+        if (abs(self.x - sprite.rect.right) <= 45 or abs(self.x - sprite.rect.left) <= 45) and self.rect.center[1] <= sprite.rect.bottom and self.rect.center[1] >= sprite.rect.top:
             self.attackDmg(sprite)
             
             
@@ -287,13 +288,13 @@ class Slime(Enemy):
 ######################
 class Bat(Enemy):
     
-    def __init__(self, screen, player,  hp = 3, dmg= 1, W_Screen = 900, H_Screen = 500, action_name = "idle"):  
+    def __init__(self, screen, player, x, y, hp = 1, dmg= 1, action_name = "idle"):  
         """_summary_
 
         Args:
             attack = hop + attack + death.
         """
-        super().__init__(player, 200, 313 - 132, hp, dmg, W_Screen, H_Screen)
+        super().__init__(player, x, y - 132, hp, dmg)
         self.screen = screen
         self.suface = {'idle' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Bat", "idle/", i))) for i in os.listdir(os.path.join("Bat", "idle")) ]
                         ,'attack' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Bat", "idlefly/", i))) for i in os.listdir(os.path.join("Bat", "idlefly")) ]
@@ -317,7 +318,7 @@ class Bat(Enemy):
                 self.kill()
             elif self.isAttack:
                 self.framerate = 0.1
-                self.action = 'idle'
+                self.action = 'death'
                 self.isAttack = False
 
             self.resetAction()
@@ -335,7 +336,7 @@ class Bat(Enemy):
             self.framerate = 0.2
             self.action = 'attack'
             self.isAttack = True 
-            self.delay = 3
+            self.delay = 4
             self.attack = True
             self.resetAction()
            
@@ -347,7 +348,9 @@ class Bat(Enemy):
         
     def attackAction(self):
         sprite = self.player.sprites()[0]
-        if abs(self.x - sprite.rect.right) <= 25 or abs(self.x - sprite.rect.left) <= 25:
+        rect = (self.suface['attack'][17]).get_rect(midtop = (self.x, self.y))
+        
+        if (abs(self.x - sprite.rect.right) <= 25 or abs(self.x - sprite.rect.left) <= 25) and rect.center[1] <= sprite.rect.bottom and rect.center[1] >= sprite.rect.top:
             self.attackDmg(sprite)     
             
     def checkCollide(self):
@@ -374,3 +377,134 @@ class Bat(Enemy):
         self.attackAction()
         #print("HP", self.getHP(), self.action, int(self.index),len(self.suface[self.action]) -1)
 
+########################
+class Golem(Enemy):
+    
+    def __init__(self, screen, player, x, y, hp = 40, dmg= 1, action_name = "idle"):  
+        """_summary_
+
+        Args:
+            attack = hop + attack + death.
+        """
+        super().__init__(player, x, y, hp, dmg)
+        self.screen = screen
+        self.suface = {'idle' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Golem", "idle/", i))) for i in os.listdir(os.path.join("Golem", "idle")) ]
+                        + [pygame.transform.scale2x(pygame.image.load(os.path.join("Golem", "glow/", i))) for i in os.listdir(os.path.join("Golem", "glow")) ]
+                        ,'attack' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Golem", "attack/", i))) for i in os.listdir(os.path.join("Golem", "attack")) ]
+                        ,'laser' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Golem", "laser/", i))) for i in os.listdir(os.path.join("Golem", "laser")) ]
+                        ,'death' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Golem", "death/", i))) for i in os.listdir(os.path.join("Golem", "death")) ]
+                        }
+
+        self.weapon = pygame.sprite.GroupSingle()
+        self.action = action_name
+        self.flip = True
+        self.image = self.suface['idle'][int(self.index)]
+        self.rect =  self.image.get_rect(midtop = (self.x, self.y))
+        self.power = 0
+        
+    def animations_state(self):
+        self.index += self.framerate
+                  
+        if int(self.index) >= len(self.suface[self.action]):
+            if self.action == 'death':
+                self.kill()
+            elif self.isAttack:
+                self.framerate = 0.1
+                self.action = 'idle'
+                self.isAttack = False
+                
+                if self.action == 'laser':
+                    self.power = 0
+
+            self.resetAction()
+            if self.delay != 0:
+                self.delay -= 1
+
+        self.image = self.suface[self.action][int(self.index)]
+        self.image = pygame.transform.flip(self.image, self.flip, False)
+        self.rect = self.image.get_rect(midtop = (self.x, self.y))  
+        #pygame.draw.rect(self.screen, 'blue', self.rect) 
+        #pygame.draw.circle(self.screen, 'red', (self.x, self.y), 3)
+        
+    def attackDmg(self, sprite):
+        if self.delay == 0:
+                     
+            self.isAttack = True 
+            self.delay = 3
+            self.attack = True
+            self.resetAction()
+            
+            if self.power == 1:
+                self.action = 'laser'
+                self.framerate = 0.05 
+            else:
+                self.action = 'attack'
+                
+            if self.action == 'attack':
+                self.power += 1
+                self.framerate = 0.2 
+           
+        if int(self.index) == 17 and self.attack and self.checkCollide():
+            sprite.setVulnarable()
+            sprite.takeDmg(self.dmg)
+            self.attack = False
+        
+        
+    def attackAction(self):
+        sprite = self.player.sprites()[0]
+        if abs(self.x - sprite.rect.right) <= 100 or abs(self.x - sprite.rect.left) <= 100:
+            self.attackDmg(sprite)     
+            
+    def checkCollide(self):
+        hits = pygame.sprite.spritecollide(self, self.player , False)#get list spire in groups
+        if hits != []:   
+            return True
+        return False
+    
+    def takeDmg(self, dmg):
+        self.setHP(self.getHP() - dmg)
+        if self.getHP() > 0:
+            self.action = 'hit'
+            self.resetAction()
+            self.istakeDmg = True
+        else:
+            self.action = 'death'
+            self.framerate = 0.05
+            if self.isLife:
+                self.resetAction()
+                self.isLife = False
+
+    def update(self):
+        self.animations_state()
+        self.attackAction()
+        print("HP", self.getHP(), self.action, self.power, int(self.index),len(self.suface[self.action]) -1)
+
+
+class Weapon(pygame.sprite.Sprite):
+    def __init__(self, screen, player, x , y, type = "shoot"):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.screen = screen
+        self.player = player
+        self.type = type
+        self.suface = {'shoot' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Golem", "shoot/", i)).convert_alpha()) for i in os.listdir(os.path.join("Golem", "shoot")) ]
+                        ,'laser' : [pygame.transform.scale2x(pygame.image.load(os.path.join("Golem", "laser_weapon/", i)).convert_alpha()) for i in os.listdir(os.path.join("Golem", "laser_weapon")) ]
+        }
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join("background", "wall" + str(index) + ".png")).convert_alpha(), (w, h))
+        self.image.set_colorkey((0, 0, 0))          
+        self.rect = self.image.get_rect(topleft = (self.x, self.y))  
+        
+    def checkCollide(self):
+        
+        if self.player.sprites() != []:
+            sprite = self.player.sprites()[0]
+            if sprite.rect.bottom - self.rect.top <= 10 and sprite.rect.bottom - self.rect.top >= 0 and sprite.rect.center[0] >= self.rect.left and sprite.rect.center[0] <= self.rect.right:
+                sprite.isJump = False
+                sprite.setLocation(sprite.x, self.rect.midtop[1])
+                sprite.jumpCount = 15
+    def update(self):
+        self.checkCollide()
+        #print(self.action, int(self.index))
+        #pygame.draw.rect(self.screen, 'blue', self.rect) 
+        
--- MeowAdventure/Agent/states.py
+++ MeowAdventure/Agent/states.py
@@ -1,12 +1,11 @@
-from re import S
 import pygame
 import os
-from enemy import Frog, Slime, Bat
+from enemy import Frog, Slime, Bat, Golem
 from wall import Wall
 from portal import Portal
 
 class States():
-    def __init__(self, screen, player, enemy, portal, wall, lv = 1):
+    def __init__(self, screen, player, enemy, portal, wall, lv = 3):
         self.lv = lv
         self.enemy = enemy
         self.player = player
@@ -34,21 +33,25 @@ class States():
             pass
         elif self.lv == 1:
             #self.enemy.add(Slime(self.screen, self.player, 500, 313))
-            self.enemy.add(Slime(self.screen, self.player, 700, 313))
+            self.enemy.add(Frog(self.screen, self.player, 700, 313))
+            
         elif self.lv == 2:
-           self.enemy.add(Frog(self.screen, self.player, 100, 313))
+           self.enemy.add(Frog(self.screen, self.player, 700, 437))
+           self.enemy.add(Bat(self.screen, self.player, 400, 355))
+           self.enemy.add(Slime(self.screen, self.player, 370, 134))
+           
            self.wall.add(Wall(self.screen, self.player, 384/1.92, 234, 494/1.92, 24/2.16, 2))
            self.wall.add(Wall(self.screen, self.player, 0, 412, 289/1.92, 411/2.16, 3))
-           self.wall.add(Wall(self.screen, self.player, 290/1.92, 412, 192/1.92, 24/2.16, 4))
+           self.wall.add(Wall(self.screen, self.player, 370, 460, 192/1.92, 24/2.16, 4))
            
+           self.wall.add(Wall(self.screen, self.player, 290/1.92, 412, 192/1.92, 24/2.16, 4))
            self.wall.add(Wall(self.screen, self.player, 287/1.92, 545, 389/1.92, 121/2.16, 5))
            self.wall.add(Wall(self.screen, self.player, 676/1.92, 575, 288/1.92, 56/2.16, 6))
            self.wall.add(Wall(self.screen, self.player, 966/1.92, 535, 765/1.92, 144/2.16, 7))
-           
            self.wall.add(Wall(self.screen, self.player, 320/1.92, 320, 212/1.92, 75/2.16, 8))
         
         elif self.lv == 3:
-            self.enemy.add(Frog(self.screen, self.player, 100, 313))
+            self.enemy.add(Golem(self.screen, self.player, 800, 335))
             self.wall.add(Wall(self.screen, self.player, 88/1.28, 435, 212/1.28, 75/1.448, 8))
             self.wall.add(Wall(self.screen, self.player, 413/1.28, 340, 228/1.28, 120/1.448, 9))
             self.wall.add(Wall(self.screen, self.player, 759/1.28, 410, 320/1.28, 73/1.448, 10))
